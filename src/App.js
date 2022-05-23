a55b59407b649151cad6a8234b1c144d30c4fea5
R-tari <95198393+R-tari@users.noreply.github.com>
2022-05-23 20:04:15 +0900

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -23,6 +23,145 @@ export const DUMMY_SRC = [
   }
 ]
 
+export const Dum_Date =
+[
+  {
+    id: 1,
+    href:'#',
+    nav: '보청기제품'   
+  },
+
+  {
+    id: 1,
+    href:'#',
+    nav: '대리점'   
+  },
+
+  {
+    id: 1,
+    href:'#',
+    nav: '미디어센터'   
+  },
+
+  {
+    id: 1,
+    href:'#',
+    nav: '고객센터'   
+  },
+
+  {
+    id: 1,
+    href:'#',
+    nav: '회사소개'   
+  },
+]
+
+export const Store_Date=
+[
+  {
+    id:'sotre1',
+    href:'#',
+    src:'/images/경주.jpg',
+    alt:'경주점',
+    storeTit:'오티콘보청기 경주점',
+    address:'경북 경주시 안강읍 안강시장길(양월리)',
+    tel:'054-761-2555',
+  },
+
+  {
+    id:'sotre2',
+    href:'#',
+    src:'/images/세종.jpg',
+    alt:'세종점',
+    storeTit:'오티콘보청기 세종점',
+    address:'세종특별자치시 나성북1로 12 (나성동, 메가타워2) 405호',
+    tel:'044-998-1239',
+  },
+
+  {
+    id:'sotre3',
+    href:'#',
+    src:'/images/노원.jpg',
+    alt:'노원점',
+    storeTit:'오티콘보청기 노원점',
+    address:'서울 노원구 한글비석로20길 43 (중계동) 2층',
+    tel:'02)-930-1234',
+  },
+
+  {
+    id:'sotre4',
+    href:'#',
+    src:'/images/조치원.jpg',
+    alt:'조치원점',
+    storeTit:'오티콘보청기 조치원점',
+    address:'세종특별자치시 조치원읍 새내로 92-1 (정리)',
+    tel:'044-998-1233',
+  },
+
+  {
+    id:'sotre5',
+    href:'#',
+    src:'/images/용산.jpg',
+    alt:'용산점',
+    storeTit:'오티콘보청기 용산점',
+    address:'서울 용산구 한강대로 109 (한강로2가, 용성비즈텔) B209호',
+    tel:'02-795-0900',
+  },
+
+  {
+    id:'sotre6',
+    href:'#',
+    src:'/images/울산.jpg',
+    alt:'울산점',
+    storeTit:'오티콘보청기 울산점',
+    address:'울산 남구 삼산로 150 (달동)',
+    tel:'052-235-5757',
+  },
+
+  {
+    id:'sotre7',
+    href:'#',
+    src:'/images/강화난천센터.jpg',
+    alt:'강화난청센터점',
+    storeTit:'오티콘보청기 강화난청센터',
+    address:'인천 강화군 강화읍 강화대로 404 (관청리)',
+    tel:'032-933-2279',
+  },
+
+  {
+    id:'sotre8',
+    href:'#',
+    src:'/images/김해부원.jpg',
+    alt:'김해부원점',
+    storeTit:'오티콘보청기 김해부원점',
+    address:'경남 김해시 김해대로 2349 (부원동, 부원역그린코아더센텀) 107동 232, 233호(부원동)',
+    tel:'055-311-8782',
+  },
+
+  {
+    id:'sotre9',
+    href:'#',
+    src:'/images/양평.jpg',
+    alt:'양평점',
+    storeTit:'오티콘보청기 양평점',
+    address:'경기 양평군 양평읍 시민로 37 (양근리)',
+    tel:'031-771-1123',
+  },
+
+  {
+    id:'sotre10',
+    href:'#',
+    src:'/images/도봉.jpg',
+    alt:'도봉점',
+    storeTit:'오티콘보청기 도봉점',
+    address:'서울 도봉구 마들로 650 (방학동, 도봉월드상가) 제 2층 제 217호',
+    tel:'02-6952-8228',
+  },
+  
+]
+
+
+
 
 
 
@@ -57,9 +196,14 @@ function Header()
                           <img src="/images/icon_sns3.png" alt="blog"/>
                       </a>
                   </li> */}
-                  {DUMMY_SRC.map((list=><list className='test' key={list.id}><a href={list.href}>
-                    <img src={list.src} alt='t'/>
-                  </a></list>))}
+                  {DUMMY_SRC.map(
+                    (
+                      list=>
+                        <list className='test' key={list.id}><a href={list.href}>
+                        <img src={list.src} alt='t'/>
+                        </a></list>
+                    )
+                  )}
                 </ul>
               </div>
             </div>
@@ -74,7 +218,7 @@ function Header()
               </h1>
               <nav class="nav">
                   <ul>
-                      <li>
+                      {/* <li>
                           <a href="#">보청기제품</a>
                       </li>
                       <li>
@@ -88,7 +232,19 @@ function Header()
                       </li>
                       <li>
                           <a href="#">회사소개</a>
-                      </li>
+                      </li> */}
+                      {Dum_Date.map(nav=>
+                      {
+                        return(
+                          <li>
+                            <a href={nav.href}>
+                            {nav.nav}
+                            </a>
+                          </li>
+                        )
+                      })
+                    }
+                    
                   </ul>
               </nav>
           </div>
@@ -129,8 +285,10 @@ function Section()
               <div className="btn next">next</div>
               <div className="btn prev">prev</div>
             </div>
-            <div className="store-contents">
-              <div className="img-box">
+            <div className="store-container">
+           {/* <div className="store-contents">
+
+               <div className="img-box">
                 <a href="#">
                 <img src="/images/경주.jpg" alt="경주점"/>
                 </a>
@@ -151,12 +309,48 @@ function Section()
                       자세히 보기
                     </div>
                   </a>
+                </div> 
+           
+              
+            </div> */}
+            {Store_Date.map(StoreCon=>
+            {
+              return(
+               <div className="store-contents">
+                  <div className="img-box">
+                    <a href={StoreCon.href}>
+                      <img src={StoreCon.src} alt={StoreCon.alt} />
+                    </a>
+                  </div>    
+
+                  <div className="info-container">
+                    <a href={StoreCon.href} className='info'>
+                      <p>
+                        <strong>{StoreCon.storeTit}</strong>
+                      </p>
+                      <address>
+                        {StoreCon.address}
+                      </address>
+                      <p className="tel">
+                        {StoreCon.tel}
+                      </p>
+                    </a>
+                    <a href="#">
+                        <div className="btn-more">
+                          자세히 보기
+                        </div>
+                      </a>
+
+                  </div>
                 </div>
-              
-              
+              )
+            }
+
+            )}
             </div>
           </div>
           
+         
         </div>
 
       
