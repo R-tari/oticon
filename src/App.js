1b3fceea58d912f74389f63152f27bbb8858e9c2
R-tari <95198393+R-tari@users.noreply.github.com>
2022-05-25 04:06:46 +0900

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -170,8 +170,72 @@ export const Search_Store=
   }
 ]
 
+export const ProductArea1=
+[
+  {
+    id:1,
+    tit:'오티콘 보청기',
+    txt:'믿을수 있는 오티콘 보청기로 새로운 세상을 만나보세요',
+    src:'/images/cic.png',
+    alt:'귓속형보청기 이미지'
+ 
+  },
+  {
+    id:2,
+    tit:'정부지원 보청기',
+    txt:'고객의 다양한 청력 유형에 맞는 총5가지의 최신 제품 라인업 구성',
+    src:'/images/보청기.png',
+    alt:'오티콘 제품 나열'
+    
+  }
+]
 
+export const ProductArea2=
+[
+  {
+    id:1,
+    txt:'오티콘 오픈(Opn)',
+    src:'/images/opn.png',
+    alt:'opn 제품 이미지'
+  },
+  {
+    id:2,
+    txt:'오티콘 모어(More)',
+    src:'/images/more.png',
+    alt:'more 제품 이미지'
+  },
+  {
+    id:3,
+    txt:'오티콘 루비',
+    src:'/images/귀걸이형.jpg',
+    alt:'귀걸이형 루비 제품 이미지'
+  },
+  
+]
 
+export const News=
+[
+  {
+    id:1,
+    href:'#',
+    txt:'오티콘,배우 김갑수 전속모델 선정'
+  },
+  {
+    id:2,
+    href:'#',
+    txt:'오티콘보청기,브랜드 전속모델로 배우 김갑수 발탁'
+  },
+  {
+    id:3,
+    href:'#',
+    txt:"오티콘보청기,프리미엄 보청기'오티콘'모델에 배우 김갑수 발탁"
+  },
+  {
+    id:4,
+    href:'#',
+    txt:"오티콘보청기, '코리아트랙 2021' 리포트 발표"
+  },
+]
 
 
 
@@ -362,6 +426,7 @@ function Section()
           
          
         </article>
+
         {Search_Store.map(SearchCon=>
           {
             return(
@@ -380,6 +445,102 @@ function Section()
               </article>
             )
           })}
+
+        <section className='bottom-section'>
+        <div className="product1">
+          {ProductArea1.map(Product1=>
+          {
+            return(
+              <article className="contents product">
+                <a href="#">
+                  <div className="out-box">
+                    <div className="in-box">
+                      <h2 className="tit">{Product1.tit}</h2>     
+                      <p>{Product1.txt}</p>
+                    </div>
+                    <div className="img-box">
+                        <img src={Product1.src} alt={Product1.alt} />
+                    </div>
+                  </div>
+                  
+                </a>
+              </article>
+            )
+          })}
+        </div>
+
+        <article className='product2'>
+          <div className="in-container">
+            <h2 className='tit'>제품소개</h2>
+            <div className="btn-box">
+              <div className="btn next">next</div>
+              <div className="btn prev">prev</div>
+            </div>
+            <div className="product-container">
+              {ProductArea2.map(Product2=>
+              {
+                return(
+                  <div className="item">
+                    <div className="img-box">
+                      <img src={Product2.src} alt={Product2.alt} />
+                    </div>
+                    <div className="name">
+                      <p>
+                        {Product2.txt}
+                      </p>
+                    </div>
+                  </div>
+                )
+              })
+             }
+            </div>
+          </div>
+        </article>
+
+        <article className="event">
+          <div className="img-box">
+            <img src="/images/b_event.jpg" alt="이벤트" />
+          </div>
+        </article>
+        </section>
+
+        <article className="news">
+          <div className="in-container">
+            <h2 className='tit'>오티콘 뉴스</h2>
+            <a href="#">
+            <div className="news-content">
+              <div className="img-box">
+                <img src="/images/news.png" alt="news이미지" />
+              </div>
+              <div className="txt-box">
+                  <p>
+                    오티콘보청기,가정의달
+                    <br />
+                    맞이 '청각 집중 케어
+                    <br />
+                    솔루션' 실시
+                  </p>
+              </div>
+            </div>
+            </a>
+            <div className="news-list">
+                <ul>
+                  {News.map(news=>
+                    {
+                      return(
+                        
+                        <li>
+                          <a href={news.href}>
+                           {news.txt}
+                          </a>
+                        </li>
+                      )
+                    })}
+                </ul>
+              </div>
+          </div>
+
+        </article>
         
 
       
