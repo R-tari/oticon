cbd5e79ca0f3dd1d2afcecc9e481fac9247910f2
R-tari <95198393+R-tari@users.noreply.github.com>
2022-06-16 19:22:15 +0900

    Add files via upload

--- src/Nav.js
+++ src/Nav.js
@@ -3,15 +3,71 @@ import {Dum_Date,SubNav1,SubNav2,SubNav3,SubNav4,SubNav5 } from './data/data_nav
 
 function Nav() {
   const [subNav,setSubNav]= useState(false);
+  
   const subNavref=useRef();
 
+  let mainNav=document.getElementsByClassName('main-nav');
+  let subUl=document.getElementsByClassName('ul_active')
+
   const NavOn=()=> {
-    setSubNav(!subNav);
+    setSubNav(true);
     console.log(subNav, 'Nav.js')
   }
 
+  const NavOff=()=> {
+    setSubNav(false);
+    console.log(subNav, 'Nav.js')
+  }
+  const NavOut=()=>
+  {
+    let i=0;
+    for(i=0;i<=mainNav.length;i++)
+    {
+      mainNav[i].style="color:#black"
+    }
+  }
+  const NavHover=()=>
+  {
+   subUl[0].addEventListener('mouseover',()=>
+    {
+      mainNav[0].style="color:#c60081"
+      console.log(mainNav[0])
+    })
+
+    subUl[1].addEventListener('mouseover',()=>
+    {
+     
+      mainNav[1].style="color:#c60081"
+      console.log(mainNav[1])
+    })
+
+    subUl[2].addEventListener('mouseover',()=>
+    {
+     
+      mainNav[2].style="color:#c60081"
+      console.log(mainNav[2])
+    })
+
+    subUl[3].addEventListener('mouseover',()=>
+    {
+     
+      mainNav[3].style="color:#c60081"
+      console.log(mainNav[3])
+    })
+
+    subUl[4].addEventListener('mouseover',()=>
+    {
+      
+      mainNav[4].style="color:#c60081"
+      console.log(mainNav[4])
+    })
+
+    
+  
+  }
+
   return (
-    <div class="nav">
+    <div class="nav" onMouseLeave={NavOff}>
           <div class="container">
               <h1 class="logo">
                  <a href="#">
@@ -22,8 +78,8 @@ function Nav() {
                   <ul>
                       {Dum_Date.map(nav=> {
                         return(
-                          <li  ref={subNavref} style={{transtion:'0.3s'}} onClick={NavOn}>
-                            <a href={nav.href}>
+                          <li   style={{transtion:'0.3s'}} >
+                            <a ref={subNavref} href={nav.href} onMouseEnter={NavOn} className="main-nav">
                             {nav.nav}
                             </a>
                           </li>
@@ -36,7 +92,7 @@ function Nav() {
           <div className="subNav" style={{height:subNav? '364px':'0',transition:'0.3s'} }>
             <div className="sub-outcontainer"style={{visibility:subNav? 'visible':'hidden',pointerEvents:subNav? 'auto':'none' }}>
               <div className="sub-incontainer" >
-                <ul className={subNav? 'ul_active':null}>
+                <ul className={subNav? 'ul_active':null} onMouseEnter={NavHover} onMouseLeave={NavOut}> 
                 {SubNav1.map(sub1=>
                   {
                     return(
@@ -49,7 +105,7 @@ function Nav() {
                   })}
                 </ul>
 
-                <ul className={subNav? 'ul_active':null}>
+                <ul className={subNav? 'ul_active':null} onMouseEnter={NavHover} onMouseLeave={NavOut}>
                 {SubNav2.map(sub2=>
                   {
                     return(
@@ -62,7 +118,7 @@ function Nav() {
                   })}
                 </ul>
 
-                <ul className={subNav? 'ul_active':null}>
+                <ul className={subNav? 'ul_active':null} onMouseEnter={NavHover} onMouseLeave={NavOut}>
                 {SubNav3.map(sub3=>
                   {
                     return(
@@ -75,7 +131,7 @@ function Nav() {
                   })}
                 </ul>
 
-                <ul className={subNav? 'ul_active':null}>
+                <ul className={subNav? 'ul_active':null} onMouseEnter={NavHover} onMouseLeave={NavOut}>
                 {SubNav4.map(sub4=>
                   {
                     return(
@@ -88,7 +144,7 @@ function Nav() {
                   })}
                 </ul>
 
-                <ul className={subNav? 'ul_active':null}>
+                <ul className={subNav? 'ul_active':null} onMouseEnter={NavHover} onMouseLeave={NavOut}>
                 {SubNav5.map(sub5=>
                   {
                     return(
